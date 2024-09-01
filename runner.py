833981df762cd65c8b2b47735858a5e7d2a6e136
pranavvb03 <139568635+pranavvb03@users.noreply.github.com>
2024-09-01 23:05:30 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -32,6 +32,7 @@ def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
     vector_store.save_local("faiss_index")
+    return vector_store
 
 def get_conversational_chain():
     prompt_template = """
