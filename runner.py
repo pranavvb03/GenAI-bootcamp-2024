213a9c60d92312b8d92694cd16567316fcc79dd7
pranavvb03 <139568635+pranavvb03@users.noreply.github.com>
2024-09-01 22:34:00 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -9,10 +9,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-
+from google.colab import userdata
 load_dotenv()
 
-genai.configure(api_key = os.getenv("GOOGLE_API_KEY")) 
+GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
 
 def get_pdf_text(pdf_docs):
     text = ""
